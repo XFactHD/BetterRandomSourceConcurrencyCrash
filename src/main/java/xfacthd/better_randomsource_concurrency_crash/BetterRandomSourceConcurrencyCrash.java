2ef71b8afaaf6bbf16b4d7e9f5285b77e7c3dea0
XFactHD <xfacthd@gmx.de>
2023-06-16 22:46:53 +0200

    Initial commit

---
+++ src/main/java/xfacthd/better_randomsource_concurrency_crash/BetterRandomSourceConcurrencyCrash.java
@@ -0,0 +1,11 @@
+package xfacthd.better_randomsource_concurrency_crash;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BetterRandomSourceConcurrencyCrash.MODID)
+public class BetterRandomSourceConcurrencyCrash
+{
+    public static final String MODID = "better_randomsource_concurrency_crash";
+
+    public BetterRandomSourceConcurrencyCrash() { }
+}
---
+++ src/main/java/xfacthd/better_randomsource_concurrency_crash/mixin/MixinLegacyRandomSource.java
@@ -0,0 +1,43 @@
+package xfacthd.better_randomsource_concurrency_crash.mixin;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.util.ThreadingDetector;
+import net.minecraft.world.level.levelgen.LegacyRandomSource;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@Mixin(LegacyRandomSource.class)
+public class MixinLegacyRandomSource
+{
+    @Unique
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    @Unique
+    private final AtomicReference<Thread> currentThread = new AtomicReference<>();
+
+    @Inject(method = "next", at = @At("HEAD"))
+    private void brscc$onNextHead(int pSize, CallbackInfoReturnable<Integer> cir)
+    {
+        Thread last = currentThread.getAndSet(Thread.currentThread());
+        if (last != null)
+        {
+            throw ThreadingDetector.makeThreadingException("LegacyRandomSource (Mixin)", last);
+        }
+    }
+
+    @Inject(method = "next", at = @At("TAIL"))
+    private void brscc$onNextTail(int pSize, CallbackInfoReturnable<Integer> cir)
+    {
+        Thread last = currentThread.getAndSet(null);
+        if (last != Thread.currentThread())
+        {
+            LOGGER.error("Thread changed unexpectedly", new Throwable());
+        }
+    }
+}
